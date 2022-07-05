a4615c2993b18ba40aed520921f48af35e607bbd
Jules Ganalon <jules.gabriel24@gmail.com>
2022-07-06 00:19:04 +0800

    Create-React-App

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+
+
+function App() {
+  return (
+    <div>
+      <header>
+      <a href="/">Tindahan</a>
+      </header>
+      <main>List of Products</main>
+    </div>
+  );
+}
+
+export default App;
