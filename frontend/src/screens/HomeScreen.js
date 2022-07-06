de06c23581ab8e8db397eee24940039b0d62463b
Jules Ganalon <jules.gabriel24@gmail.com>
2022-07-07 00:07:43 +0800

    updates

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,37 +1,68 @@
-import React, { useEffect, useState } from "react";
-import { Link } from "react-router-dom";
+import React, { useEffect, useReducer } from "react";
 import axios from 'axios';
+import logger from 'use-reducer-logger';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Product from "../components/product";
+import { Helmet } from "react-helmet-async";
+import LoadingBox from "../components/loadingbox";
+import MessageBox from "../components/messagebox";
 //import data from "../data";
 
+const reducer = (state, action) => {
+  switch(action.type) {
+    case 'FETCH_REQUEST':
+      return {...state, loading: true};
+    case 'FETCH_SUCCESS':
+      return {...state,products: action.payload, loading: false}
+    case 'FETCH_FAIL':
+      return {...state, loading: false, error: action.payload}
+    default:
+      return state;
+  }};
+
 function HomeScreen() {
-  const [products, setProducts] = useState([]);
+  const [{ loading, error, products }, dispatch] = useReducer(logger(reducer), {
+    products: [],
+    loading: true,
+    error: "",
+  });
+  //const [products, setProducts] = useState([]);
   useEffect(() => {
     const fetchData = async () => {
-      const result = await axios.get('/api/products');
-      setProducts(result.data);
+      dispatch({ type: "FETCH_REQUEST" });
+      try {
+        const result = await axios.get("/api/products");
+        dispatch({ type: "FETCH_SUCCESS", payload: result.data });
+      } catch (err) {
+        dispatch({ type: "FETCH_FAIL", payload: err.message });
+      }
+      //setProducts(result.data);
     };
     fetchData();
-  }, []);
+  }, []); 
 
-    return(
-     <div>
-        <h1>Featured Products</h1>
-          <div className="products">
-          {products.map(product =>(
-          <div className="product" key={product.slug}>
-            <Link to={`/product/${product.slug}`}>
-            <img src={product.image} alt={product.name} height="829" width="679" />
-            </Link>
-              <div className="product-info">
-                <p><Link to={`/product/${product.slug}`}>{product.name}</Link>
-                </p>
-                <p><strong>${product.price}</strong></p>
-              </div>
-            </div>
-          ))}
-        </div>
+  return (
+    <div>
+      <Helmet><title>Tindahan</title></Helmet>
+      <h1>Featured Products</h1>
+      <div className="products">
+        {loading ? (
+            <LoadingBox/>
+        ) : error ? (
+            <MessageBox variant="danger">{error}</MessageBox>
+        ) : (
+          <Row>
+            {products.map((product) => (
+              <Col key={product.slug} sm={6} md={4} lg={3} className="mb-3">
+                <Product product={product}></Product>
+              </Col>
+            ))}
+          </Row>
+        )}
+      </div>
     </div>
-    );
+  );
 }
 
 export default HomeScreen;
